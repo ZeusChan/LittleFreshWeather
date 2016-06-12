3171ccb49ba3daf69daa3e748fb6c05ce976821c
ZeusChan <zeuschan@126.com>
2016-06-12 21:42:08 +0800

    Add the CityWeatherActivity.

--- presentation/src/main/java/com/zeuschan/littlefreshweather/prsentation/views/activity/CityWeatherActivity.java
+++ presentation/src/main/java/com/zeuschan/littlefreshweather/prsentation/views/activity/CityWeatherActivity.java
@@ -2,40 +2,71 @@ package com.zeuschan.littlefreshweather.prsentation.views.activity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.zeuschan.littlefreshweather.model.entities.WeatherEntity;
 import com.zeuschan.littlefreshweather.prsentation.R;
+import com.zeuschan.littlefreshweather.prsentation.presenters.CityWeatherPresenter;
 import com.zeuschan.littlefreshweather.prsentation.views.CityWeatherView;
+import com.zeuschan.littlefreshweather.prsentation.views.adapter.CityWeatherAdapter;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CityWeatherActivity extends BaseActivity implements CityWeatherView {
+
+    public static final String TAG = CityWeatherActivity.class.getSimpleName();
+    private CityWeatherPresenter mPresenter;
+    private CityWeatherAdapter mCityWeatherAdapter;
+
+    @BindView(R.id.rl_loading_progress) RelativeLayout rlLoadingProgress;
+    @BindView(R.id.rl_failed_retry) RelativeLayout rlFailedRetry;
+    @BindView(R.id.rv_city_weather) RecyclerView rvCityWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_weather);
         ButterKnife.bind(this);
+
+        mPresenter = new CityWeatherPresenter();
+        mPresenter.attachView(this);
+
+        rvCityWeather.setLayoutManager(new LinearLayoutManager(this));
+        mCityWeatherAdapter = new CityWeatherAdapter();
+        rvCityWeather.setAdapter(mCityWeatherAdapter);
+
+        mPresenter.start();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     @Override
     public void renderCityWeather(WeatherEntity entity) {
-
+        if (entity != null) {
+            mCityWeatherAdapter.setWeatherEntity(entity);
+        }
     }
 
     @Override
     public Context getContext() {
-        return null;
+        return this;
     }
 
     @Override
     public void showLoading() {
-
+        rlLoadingProgress.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hideLoading() {
-
+        rlLoadingProgress.setVisibility(View.GONE);
     }
 
     @Override
@@ -50,11 +81,17 @@ public class CityWeatherActivity extends BaseActivity implements CityWeatherView
 
     @Override
     public void showRetry() {
-
+        rlFailedRetry.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hideRetry() {
+        rlFailedRetry.setVisibility(View.GONE);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mPresenter.stop();
     }
 }
---
+++ presentation/src/main/java/com/zeuschan/littlefreshweather/prsentation/views/adapter/CityWeatherAdapter.java
@@ -0,0 +1,57 @@
+package com.zeuschan.littlefreshweather.prsentation.views.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.zeuschan.littlefreshweather.model.entities.WeatherEntity;
+import com.zeuschan.littlefreshweather.prsentation.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by chenxiong on 2016/6/12.
+ */
+public class CityWeatherAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    private WeatherEntity weatherEntity = null;
+
+    public void setWeatherEntity(WeatherEntity weatherEntity) {
+        this.weatherEntity = weatherEntity;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return weatherEntity != null ? 1 : 0;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new MainViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.cv_city_weather_main, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (weatherEntity != null) {
+            MainViewHolder mainViewHolder = (MainViewHolder)holder;
+            mainViewHolder.tvCityName.setText(weatherEntity.getCityName());
+            mainViewHolder.tvAirQualityIndex.setText(weatherEntity.getAirQulityIndex() + "");
+            mainViewHolder.tvCurTemp.setText(weatherEntity.getCurrentTemperature() + "");
+        }
+    }
+
+    public static class MainViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tv_city_weather_main_city_name) TextView tvCityName;
+        @BindView(R.id.tv_city_weather_main_air_quality_index) TextView tvAirQualityIndex;
+        @BindView(R.id.tv_city_weather_main_cur_temp) TextView tvCurTemp;
+
+        public MainViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
